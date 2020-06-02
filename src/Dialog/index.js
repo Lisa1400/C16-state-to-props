f31c7baae333394127822c1fd53a50f8f322ddd1
Lisa1400 <lisamabe631@gmail.com>
2020-06-02 12:59:00 +0200

    added component

---
+++ src/Dialog/index.js
@@ -0,0 +1,35 @@
+import React from "react";
+import Presentation from "./Presentation";
+
+export default function ScrollDialog() {
+  const [open, setOpen] = React.useState(false);
+  const [scroll, setScroll] = React.useState("paper");
+
+  const handleClickOpen = (scrollType) => () => {
+    setOpen(true);
+    setScroll(scrollType);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const descriptionElementRef = React.useRef(null);
+  React.useEffect(() => {
+    if (open) {
+      const { current: descriptionElement } = descriptionElementRef;
+      if (descriptionElement !== null) {
+        descriptionElement.focus();
+      }
+    }
+  }, [open]);
+  return (
+    <Presentation
+      handleClickOpen={handleClickOpen}
+      handleClose={handleClose}
+      scroll={scroll}
+      descriptionElementRef={descriptionElementRef}
+      open={open}
+    />
+  );
+}
